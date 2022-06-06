ca6d60b9d30a902e1e711ab6eb8ef9fd7d0867c9
teacher <teacher@egsk.com.tw>
2022-06-06 09:55:39 +0800

    update

--- WebSite_ADO/App_Code/CustomerUtility.cs
+++ WebSite_ADO/App_Code/CustomerUtility.cs
@@ -39,4 +39,23 @@ ConfigurationManager.ConnectionStrings["NorthwindConnectionString1"].ConnectionS
 
         return customers;
     }
+
+    public void Insert(string customerId , string companyName , string country , string city)
+    {
+        SqlConnection sqlConnection = new SqlConnection(this.ConnectionString);
+
+        SqlCommand sqlCommand = new SqlCommand(
+            "insert into Customers (CustomerID , CompanyName , Country , City) " +
+            "values(@CustomerID , @CompanyName , @Country , @City)",
+            sqlConnection);
+
+        sqlCommand.Parameters.AddWithValue("@CustomerID", customerId);
+        sqlCommand.Parameters.AddWithValue("@CompanyName", companyName);
+        sqlCommand.Parameters.AddWithValue("@Country", country);
+        sqlCommand.Parameters.AddWithValue("@City", city);
+
+        sqlConnection.Open();
+        sqlCommand.ExecuteNonQuery();
+        sqlConnection.Close();
+    }
 }

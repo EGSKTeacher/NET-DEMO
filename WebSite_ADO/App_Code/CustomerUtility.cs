9e58484f7b35f35f0ef823178e84aff82a104f37
teacher <teacher@egsk.com.tw>
2022-06-06 09:23:46 +0800

    update

---
+++ WebSite_ADO/App_Code/CustomerUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+
+public class CustomerUtility
+{
+    private string ConnectionString;
+    public CustomerUtility()
+    {
+        ConnectionString =
+ConfigurationManager.ConnectionStrings["NorthwindConnectionString1"].ConnectionString;
+    }
+}

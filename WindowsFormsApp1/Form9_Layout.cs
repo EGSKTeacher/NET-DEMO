7fb390d690d8905c325ea9bacb8e81f3643e8dc6
teacher <teacher@egsk.com.tw>
2022-06-07 14:25:41 +0800

    update

---
+++ WindowsFormsApp1/Form9_Layout.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form9_Layout : Form
+    {
+        public Form9_Layout()
+        {
+            InitializeComponent();
+        }
+    }
+}

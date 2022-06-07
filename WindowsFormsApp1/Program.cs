9d0705d56a3de040e28a1bfb0d5cb329b97f9807
teacher <teacher@egsk.com.tw>
2022-06-07 13:43:26 +0800

    update

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form4_ListBox());
+            Application.Run(new Form5_FlowLayoutPanel());
         }
     }
 }

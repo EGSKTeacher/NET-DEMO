9e344c2f51494c52cd71e8afbe7a083ba939c26b
teacher <teacher@egsk.com.tw>
2022-06-07 16:53:53 +0800

    update

--- WindowsFormsApp1/Form14_MaskedTextBox.Designer.cs
+++ WindowsFormsApp1/Form14_MaskedTextBox.Designer.cs
@@ -32,6 +32,7 @@ namespace WindowsFormsApp1
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
             this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // maskedTextBox1
@@ -50,6 +51,7 @@ namespace WindowsFormsApp1
             this.maskedTextBox2.Size = new System.Drawing.Size(100, 22);
             this.maskedTextBox2.TabIndex = 1;
             this.maskedTextBox2.ValidatingType = typeof(System.DateTime);
+            this.maskedTextBox2.Validating += new System.ComponentModel.CancelEventHandler(this.maskedTextBox2_Validating);
             // 
             // maskedTextBox3
             // 
@@ -59,11 +61,21 @@ namespace WindowsFormsApp1
             this.maskedTextBox3.Size = new System.Drawing.Size(100, 22);
             this.maskedTextBox3.TabIndex = 2;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(243, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
             // Form14_MaskedTextBox
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.maskedTextBox3);
             this.Controls.Add(this.maskedTextBox2);
             this.Controls.Add(this.maskedTextBox1);
@@ -79,5 +91,6 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private System.Windows.Forms.MaskedTextBox maskedTextBox2;
         private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.Label label1;
     }
 }
--- WindowsFormsApp1/Form14_MaskedTextBox.cs
+++ WindowsFormsApp1/Form14_MaskedTextBox.cs
@@ -16,6 +16,21 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
         //https://docs.microsoft.com/zh-tw/dotnet/api/system.windows.forms.maskedtextbox.mask?view=netframework-4.7.2&f1url=%3FappId%3DDev16IDEF1%26l%3DEN-US%26k%3Dk(System.Windows.Forms.MaskedTextBox.Mask)%3Bk(System.Windows.Forms.MaskedTextBox)%3Bk(VS.Properties)%3Bk(TargetFrameworkMoniker-.NETFramework%2CVersion%253Dv4.7.2)%26rd%3Dtrue
+        private void maskedTextBox2_Validating(object sender, CancelEventArgs e)
+        {
+            //maskedTextBox2.Text
+            DateTime d;
+            if (DateTime.TryParse(maskedTextBox2.Text , out d))
+            {
+                label1.Text = "OK";
+            }
+            else
+            {
+                label1.Text = "Error";
+            }
+
+        }
     }
 }

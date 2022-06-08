63899e7ece00b8ec29d63cb95dc98e52eb2928a9
teacher <teacher@egsk.com.tw>
2022-06-08 10:25:05 +0800

    update

---
+++ WindowsFormsApp1/Form18_DataBinding.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form18_DataBinding : Form
+    {
+        public Form18_DataBinding()
+        {
+            InitializeComponent();
+        }
+
+        private void Form18_DataBinding_Load(object sender, EventArgs e)
+        {
+            List<Member> members = new List<Member>()
+            {
+                new Member(){ Id=1 , Name="mary" },
+                new Member(){ Id=2 , Name="john" },
+                new Member(){ Id=3 , Name="andy" }
+            };
+
+            comboBox1.DataSource = members;
+            //comboBox1.DataBind()
+
+            comboBox1.DisplayMember = "Name";
+            comboBox1.ValueMember = "Id";
+        }
+    }
+
+    class Member
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form17_CheckedListBox());
+            Application.Run(new Form18_DataBinding());
         }
     }
 }

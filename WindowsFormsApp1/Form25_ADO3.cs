29e98a9b87bd9a7329ac3729fa4ea56d4c19b7f6
teacher <teacher@egsk.com.tw>
2022-06-08 15:59:26 +0800

    update

---
+++ WindowsFormsApp1/Form25_ADO3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form25_ADO3 : Form
+    {
+        public Form25_ADO3()
+        {
+            InitializeComponent();
+        }
+
+        private SqlDataAdapter sqlDataAdapter;
+        private DataSet ds;
+
+        private void Form25_ADO3_Load(object sender, EventArgs e)
+        {
+            sqlDataAdapter = new SqlDataAdapter(
+               "select CustomerId , CompanyName , Country , City from Customers",
+               ConfigurationManager.ConnectionStrings["NorthwindConnectionString1"].ConnectionString
+               );
+
+            ds = new DataSet();
+
+            sqlDataAdapter.Fill(ds , "Customers");
+
+            dataGridView1.DataSource = ds;
+            dataGridView1.DataMember = "Customers";
+        }
+    }
+}
--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form24_ADO2());
+            Application.Run(new Form25_ADO3());
         }
     }
 }

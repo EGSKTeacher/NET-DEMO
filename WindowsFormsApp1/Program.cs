ccfebb93ac77fdfce128a6c90d2d7705fccc4039
teacher <teacher@egsk.com.tw>
2022-06-08 11:02:42 +0800

    update

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form18_DataBinding());
+            Application.Run(new Form19_TrackBar());
         }
     }
 }

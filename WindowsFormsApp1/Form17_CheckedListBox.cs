714b60609afa8ffa61832f5486d9ea34da764785
teacher <teacher@egsk.com.tw>
2022-06-08 10:09:47 +0800

    update

---
+++ WindowsFormsApp1/Form17_CheckedListBox.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form17_CheckedListBox : Form
+    {
+        public Form17_CheckedListBox()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var item in checkedListBox1.CheckedItems)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form16_ComboBox());
+            Application.Run(new Form17_CheckedListBox());
         }
     }
 }
